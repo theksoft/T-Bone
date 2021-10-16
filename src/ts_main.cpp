7df514810c4af4496280b166241913ca2d41f415
thek <soft.thek@gmail.com>
2021-10-16 23:27:26 +0200

    Add command line options

--- src/ts_main.cpp
+++ src/ts_main.cpp
@@ -2,37 +2,154 @@
 #include "ts_tee_settings.hpp"
 #include <iostream>
 
-using namespace tbone::server;
+//==============================================================================
+
+namespace tbone::server {
+
+class Options {
+
+public:
+  Options(int argc, char* argv[])
+    : _command(""),
+      _error(false),
+      _help(false),
+      _errorMessage(""),
+      _configurationFile(TEE_DEFAULT_CONFIG_FILENAME) {
+
+    if (argc) { _command = argv[0]; }
+
+    int c;
+    while (!_error && -1 != (c = getopt (argc, argv, "hc:"))) {
+      switch (c) {
+      case 'h': _help = true; break;
+      case 'c':
+        _configurationFile = optarg;
+        if (!strlen(optarg)) {
+          _errorMessage = "ERROR Option 'c' requires a valid argument.";
+          _error = true;
+        }
+        break;
+      case '?':
+        if (optopt == 'c')
+          _errorMessage = "ERROR Option '-" + std::string(1, (char)optopt) + "' requires an argument.";
+        else
+          _errorMessage = "Unknown option '-" + std::string(1, (char)optopt) + "'.";
+        _error = true;
+        break;
+      default:
+        _errorMessage = "Invalid command line!";
+        _error = true;
+        break;
+      }
+    }
+  }
+
+  ~Options() {
+    clear();
+  }
+
+  void printError() {
+    std::cerr << std::endl << _errorMessage << std::endl;
+  }
+
+  void printUsage() {
+    std::cout << std::endl;
+    std::cout << "Usage: " << _command << " [options]" << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "  -c <filepath>  <filepath> for configuration file." << std::endl
+              << "                 Default is \"" << TEE_DEFAULT_CONFIG_FILENAME << "\".)" << std::endl;
+    std::cout << "  -h             Display this help and exit." << std::endl;
+    std::cout << std::endl;
+  }
+
+  bool isValid() const                            { return !_error; }
+  bool needHelp() const                           { return _help; }
+  const std::string& getConfigurationFile() const { return _configurationFile; }
+
+private:
+  void clear() {
+    _command.clear();
+    _error = _help = false;
+    _errorMessage.clear();
+    _configurationFile.clear();
+  }
+
+private:
+  std::string _command;
+  bool _error;
+  bool _help;
+  std::string _errorMessage;
+  std::string _configurationFile;
+};
+
+}   // namespace tbone::server
+
+//==============================================================================
 
 int main(int argc, char* argv[])
 {
-  (void)argc; (void)argv;
-  TeeSettings settings;
-  if (!settings.readFile()) {
+  using namespace tbone::server;
+
+  Options opt(argc, argv);
+  if (opt.needHelp() || !opt.isValid()) {
+    if (!opt.isValid())
+      opt.printError();
+    opt.printUsage();
     return 0;
   }
+
+  TeeSettings settings;
+  if (!settings.readFile(opt.getConfigurationFile())) {
+    return 0;
+  }
+
+  AppLocalServer *als = NULL;
+  AppTcpServer *ats = NULL;
+
   try
   {
+    bool oneOfAppServiceSet = false;
     std::string file = settings.getApplicationLocalFile();
-    ::unlink(file.c_str());
+    if (!file.empty()) {
+      ::unlink(file.c_str());
+      als = new AppLocalServer(
+        TeeIOContext::get()->getIOContext(),
+        bstlocal::stream_protocol::endpoint(file)
+      );
+      oneOfAppServiceSet = true;
+    }
 
-    AppLocalServer  als (
-      TeeIOContext::get()->getIOContext(),
-      bstlocal::stream_protocol::endpoint(file)
-    );
+    unsigned short port = settings.getApplicationPort();
+    if (0 != port) {
+      ats = new AppTcpServer(
+        TeeIOContext::get()->getIOContext(),
+        bstip::tcp::endpoint(bstip::tcp::v4(), port)
+      );
+      oneOfAppServiceSet = true;
+    }
 
-    AppTcpServer ats(
-      TeeIOContext::get()->getIOContext(),
-      bstip::tcp::endpoint(bstip::tcp::v4(), settings.getApplicationPort())
-    );
+    port = settings.getInpectUrlPort();
+    if (0 != port) {
+      std::cout << "INFO Inspect service not implemented!" << std::endl;
+    }
 
-    TeeIOContext::get()->getIOContext().run();
+    if (oneOfAppServiceSet) {
+      TeeIOContext::get()->getIOContext().run();
+    } else {
+      std::cerr << "No application service defines!" << std::endl;
+    }
   }
   catch (std::exception& e)
   {
     std::cerr << "Exception: " << e.what() << "\n";
   }
 
+  if (als) delete als;
+  if (ats) delete ats;
   std::cout << std::endl << "<<<=== TEE services stopped! ===>>>" << std::endl << std::endl;
   return 0;
 }
+
+//==============================================================================
+
+// EOF
